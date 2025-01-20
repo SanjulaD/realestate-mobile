d7fa67fa29ebe5129790dc0623bcaad29357882d
SanjulaD <desanjula99@gmail.com>
2025-01-21 00:00:15 +0530

    feat: add global contect provider

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,10 +1,28 @@
-import { View, Text, SafeAreaView, ScrollView, Image, TouchableOpacity } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView, Image, TouchableOpacity, Alert } from 'react-native'
 import React from 'react'
 import images from '@/constants/images'
 import icons from '@/constants/icons'
+import { login } from '@/lib/appwrite'
+import { useGlobalContext } from '@/lib/global-provider'
+import { Redirect } from 'expo-router'
 
 const SignIn = () => {
-  const handleLogin = () => {}
+
+  const { refetch, loading, isLogged } = useGlobalContext()
+
+  if (!loading && isLogged) {
+    return <Redirect href="/" />
+  }
+
+  const handleLogin = async () => {
+    const result = await login();
+
+    if (result) {
+      refetch();
+    } else {
+      Alert.alert('Error', 'Failed to login');
+    }
+  }
 
   return (
     <SafeAreaView className="h-full bg-white">
