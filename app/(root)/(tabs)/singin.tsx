e4992b5daf71cddbf0b2b3558bf66152be136cc6
SanjulaD <desanjula99@gmail.com>
2025-01-20 23:13:01 +0530

    feat: added ui for login screen

--- app/(root)/(tabs)/singin.tsx
+++ app/(root)/(tabs)/singin.tsx
@@ -4,23 +4,25 @@ import images from '@/constants/images'
 import icons from '@/constants/icons'
 
 const SignIn = () => {
-
-  const handleLogin = () => { }
+  const handleLogin = () => {}
 
   return (
-    <SafeAreaView className='bg-white h-full'>
-      <ScrollView contentContainerClassName='h-full'>
-        <Image source={images.onboarding} className='w-full h-4/6' resizeMode='contain' />
-        <View className='px-10'>
-          <Text className='text-base text-center uppercase font-rubik text-black-200'>Welcome to Real Estate</Text>
-          <Text className='mt-2 text-3xl font-rubik-bold text-black-300 text-center'>Let's Get You Closer to {"\n"}
-            <Text className='text-primary-300'>Your Ideal Home</Text>
+    <SafeAreaView className="h-full bg-white">
+      <ScrollView contentContainerClassName="h-full">
+        <Image source={images.onboarding} className="h-4/6 w-full" resizeMode="contain" />
+        <View className="px-10">
+          <Text className="text-center font-rubik text-base uppercase text-black-200">Welcome to Real Estate</Text>
+          <Text className="mt-2 text-center font-rubik-bold text-3xl text-black-300">
+            Let's Get You Closer to {'\n'}
+            <Text className="text-primary-300">Your Ideal Home</Text>
           </Text>
-          <Text className='text-lg font-rubik text-black-200 text-center mt-12'>Login to Real Estate with Google</Text>
-          <TouchableOpacity onPress={handleLogin} className='bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5'>
-            <View className='flex flex-row items-center justify-center'>
-              <Image source={icons.google} className='w-5 h-5' resizeMode='contain' />
-              <Text className='ml-2 text-lg font-rubik-medium text-black-300'>Login with Google</Text>
+          <Text className="mt-12 text-center font-rubik text-lg text-black-200">Login to Real Estate with Google</Text>
+          <TouchableOpacity
+            onPress={handleLogin}
+            className="mt-5 w-full rounded-full bg-white py-4 shadow-md shadow-zinc-300">
+            <View className="flex flex-row items-center justify-center">
+              <Image source={icons.google} className="h-5 w-5" resizeMode="contain" />
+              <Text className="ml-2 font-rubik-medium text-lg text-black-300">Login with Google</Text>
             </View>
           </TouchableOpacity>
         </View>
