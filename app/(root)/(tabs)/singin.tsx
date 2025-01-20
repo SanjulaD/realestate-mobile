04b7fde293e63e7fca07ef22d44753b0126f7856
SanjulaD <desanjula99@gmail.com>
2025-01-20 23:11:29 +0530

    feat: added ui for login screen

---
+++ app/(root)/(tabs)/singin.tsx
@@ -0,0 +1,32 @@
+import { View, Text, SafeAreaView, ScrollView, Image, TouchableOpacity } from 'react-native'
+import React from 'react'
+import images from '@/constants/images'
+import icons from '@/constants/icons'
+
+const SignIn = () => {
+
+  const handleLogin = () => { }
+
+  return (
+    <SafeAreaView className='bg-white h-full'>
+      <ScrollView contentContainerClassName='h-full'>
+        <Image source={images.onboarding} className='w-full h-4/6' resizeMode='contain' />
+        <View className='px-10'>
+          <Text className='text-base text-center uppercase font-rubik text-black-200'>Welcome to Real Estate</Text>
+          <Text className='mt-2 text-3xl font-rubik-bold text-black-300 text-center'>Let's Get You Closer to {"\n"}
+            <Text className='text-primary-300'>Your Ideal Home</Text>
+          </Text>
+          <Text className='text-lg font-rubik text-black-200 text-center mt-12'>Login to Real Estate with Google</Text>
+          <TouchableOpacity onPress={handleLogin} className='bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5'>
+            <View className='flex flex-row items-center justify-center'>
+              <Image source={icons.google} className='w-5 h-5' resizeMode='contain' />
+              <Text className='ml-2 text-lg font-rubik-medium text-black-300'>Login with Google</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default SignIn
--- app/index.tsx
+++ app/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         alignItems: 'center',
       }}>
       <Text className="my-10 font-rubik text-3xl font-bold">Welcome to Real Estate</Text>
-      <Link href="/sign-in">Sign In</Link>
+      <Link href="/signin">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
       <Link href="/property/1">Property</Link>
