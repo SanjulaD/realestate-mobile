925fd6308330e4bbbfba8cd24d0ea88a673882f8
SanjulaD <desanjula99@gmail.com>
2025-01-27 23:03:02 +0530

    feat: profile screen

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Index = () => {
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}>
+            <Text className="my-10 font-rubik text-3xl font-bold">Welcome to Real Estate</Text>
+        </View>
+    )
+}
+
+export default Index
