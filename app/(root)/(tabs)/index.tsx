457e5bdcb7edd48d4fae6dcc7aa697b0de4e1dac
SanjulaD <desanjula99@gmail.com>
2025-01-28 23:08:52 +0530

    feat: profile search

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,17 +1,29 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Image } from 'react-native'
+import images from '@/constants/images'
+import icons from '@/constants/icons'
+import Search from '@/components/Search'
 
 const Index = () => {
-    return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-            }}>
-            <Text className="my-10 font-rubik text-3xl font-bold">Welcome to Real Estate</Text>
+  return (
+    <SafeAreaView className="h-full bg-white">
+      <View className='px-5'>
+        <View className='flex flex-row items-center justify-between mt-5'>
+          <View className='flex flex-row items-center'>
+            <Image source={images.avatar} className='size-12 rounded-full' />
+            <View className='flex flex-col items-start ml-2 justify-center'>
+              <Text className='text-xs font-rubik text-black-100'>Good Morning!</Text>
+              <Text className='text-base font-rubik-medium text-black-300'>Sanjula De Alwis</Text>
+            </View>
+          </View>
+          <Image source={icons.bell} className='size-6' />
         </View>
-    )
+      </View>
+      <Search />
+    </SafeAreaView>
+  )
 }
 
 export default Index
