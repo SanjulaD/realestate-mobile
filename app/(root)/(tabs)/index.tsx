73b39de0d596eb1a09ba1db0358b9aee35eddc5b
SanjulaD <desanjula99@gmail.com>
2025-01-28 23:09:13 +0530

    feat: profile search

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -9,16 +9,16 @@ import Search from '@/components/Search'
 const Index = () => {
   return (
     <SafeAreaView className="h-full bg-white">
-      <View className='px-5'>
-        <View className='flex flex-row items-center justify-between mt-5'>
-          <View className='flex flex-row items-center'>
-            <Image source={images.avatar} className='size-12 rounded-full' />
-            <View className='flex flex-col items-start ml-2 justify-center'>
-              <Text className='text-xs font-rubik text-black-100'>Good Morning!</Text>
-              <Text className='text-base font-rubik-medium text-black-300'>Sanjula De Alwis</Text>
+      <View className="px-5">
+        <View className="mt-5 flex flex-row items-center justify-between">
+          <View className="flex flex-row items-center">
+            <Image source={images.avatar} className="size-12 rounded-full" />
+            <View className="ml-2 flex flex-col items-start justify-center">
+              <Text className="font-rubik text-xs text-black-100">Good Morning!</Text>
+              <Text className="font-rubik-medium text-base text-black-300">Sanjula De Alwis</Text>
             </View>
           </View>
-          <Image source={icons.bell} className='size-6' />
+          <Image source={icons.bell} className="size-6" />
         </View>
       </View>
       <Search />
--- components/Search.tsx
+++ components/Search.tsx
@@ -1,41 +1,40 @@
 import { View, Image, TextInput, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
-import { router, useLocalSearchParams, usePathname } from 'expo-router';
-import { useDebouncedCallback } from "use-debounce";
-import icons from '@/constants/icons';
+import { router, useLocalSearchParams, usePathname } from 'expo-router'
+import { useDebouncedCallback } from 'use-debounce'
+import icons from '@/constants/icons'
 
 const Search = () => {
+  const path = usePathname()
+  const params = useLocalSearchParams<{ query?: string }>()
+  const [search, setSearch] = useState(params.query)
 
-    const path = usePathname();
-    const params = useLocalSearchParams<{ query?: string }>();
-    const [search, setSearch] = useState(params.query);
+  const debouncedSearch = useDebouncedCallback((text: string) => {
+    router.setParams({ query: text })
+  }, 500)
 
-    const debouncedSearch = useDebouncedCallback((text: string) => {
-        router.setParams({ query: text });
-    }, 500);
+  const handleSearch = (text: string) => {
+    setSearch(text)
+    debouncedSearch(text)
+  }
 
-    const handleSearch = (text: string) => {
-        setSearch(text);
-        debouncedSearch(text);
-    };
+  return (
+    <View className="mt-5 flex w-full flex-row items-center justify-between rounded-lg border border-primary-100 bg-accent-100 px-4 py-2">
+      <View className="z-50 flex flex-1 flex-row items-center justify-start">
+        <Image source={icons.search} className="size-5" />
+        <TextInput
+          value={search}
+          onChangeText={handleSearch}
+          placeholder="Search for anything"
+          className="ml-2 flex-1 font-rubik text-sm text-black-300"
+        />
+      </View>
 
-    return (
-        <View className="flex flex-row items-center justify-between w-full px-4 rounded-lg bg-accent-100 border border-primary-100 mt-5 py-2">
-            <View className="flex-1 flex flex-row items-center justify-start z-50">
-                <Image source={icons.search} className="size-5" />
-                <TextInput
-                    value={search}
-                    onChangeText={handleSearch}
-                    placeholder="Search for anything"
-                    className="text-sm font-rubik text-black-300 ml-2 flex-1"
-                />
-            </View>
-
-            <TouchableOpacity>
-                <Image source={icons.filter} className="size-5" />
-            </TouchableOpacity>
-        </View>
-    )
+      <TouchableOpacity>
+        <Image source={icons.filter} className="size-5" />
+      </TouchableOpacity>
+    </View>
+  )
 }
 
 export default Search
